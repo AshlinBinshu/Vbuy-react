f16de90c2673c779f811978817050511045dc891
AshlinBinshu <ashlin.jayan@gmail.com>
2024-07-27 22:31:43 +0530

    navbar and searchbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,23 @@
+import {Link} from "react-router-dom"
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+  return(
+    <nav className="flex flex-col sm:flex-row justify-between items-start sm:items-center px-5 py-5
+     fixed top-0 left-0 right-0 shadow md gap-1 sm:gap-0 z-30 bg-white">
+    {/* Logo and site */}
+     <div className="flex justify-between items-center">
+        {/* Add image later */}
+        <Link to="/" className="font-bold text-3xl">Vbuy</Link>
+     </div>
+
+     {/* list of other tabs */}
+     <ul className="flex gap-6 text-lg font font-semibold text-gray-400">
+        <Link to="/" className="hover:text-black cursor-pointer sm:p-2">About</Link>
+        <Link to="/" className="hover:text-black cursor-pointer sm:p-2">Contact</Link>
+        <Link to="/" className="hover:text-black cursor-pointer sm:p-2">Login</Link>
+        <Link to="/" className="hover:text-black cursor-pointer sm:p-2">Sign up</Link>
+     </ul>
+    </nav>
+  ); 
 }
 
 export default Navbar
